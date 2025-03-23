084c63cc7b2d29f2c54bca614f80c9f29ce863e7
Martin Sundhaug <martinsundhaug@gmail.com>
2025-03-23 17:28:09 +0000

    Splash!

--- source/startup.c
+++ source/startup.c
@@ -6,28 +6,21 @@
 #include <ogc/lwp_watchdog.h>
 
 #include "FreeMonoBold_ttf.h"
+#include "title_png.h"
 
 int main(int argc, char **argv) {
     GRRLIB_Init();
     WPAD_Init();
 
     GRRLIB_ttfFont *myFont = GRRLIB_LoadTTF(FreeMonoBold_ttf, FreeMonoBold_ttf_size);
-    GRRLIB_texImg *CopiedImg = GRRLIB_CreateEmptyTexture(rmode->fbWidth, rmode->efbHeight);
+    GRRLIB_texImg *titleImg = GRRLIB_LoadTexture(title_png);
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
 
     srand(time(NULL));
 
     int frameCount = 0;
     while(true) {
-        GRRLIB_DrawImg(0, 0, CopiedImg, 0, 1, 1, 0xFFFFFFFF);
-        GRRLIB_PrintfTTF(100, 100, myFont, "Reentrii", 32, 0xFFFFFFFF);
-        // GRRLIB_Screen2Texture(0,0, CopiedImg, false);
-
-        for (int x = 0; x < rmode->fbWidth; x++) {
-                for (int y = 0; y < rmode->efbHeight; y++) {
-                    GRRLIB_SetPixelTotexImg(x, y, CopiedImg, (rand() % 0xFFFFFF) << 8 | 0xFF);
-                }
-            }
+        GRRLIB_DrawImg(0, 0, titleImg, 0, 1, 1, 0xFFFFFFFF);
 
         WPAD_ScanPads();
         u32 pressed = WPAD_ButtonsDown(0);
