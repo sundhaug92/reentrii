2bbcea406ab5d2d872920d4eebeb1ea36de16ab8
Martin Sundhaug <martinsundhaug@gmail.com>
2025-03-24 02:57:20 +0000

    Fix error screen spamming (esp nul messages)

--- source/error_screen.c
+++ source/error_screen.c
@@ -2,18 +2,23 @@
 #include <grrlib.h>
 #include <wiiuse/wpad.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 GameModeExit error_screen(GameState* state) {
     GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
 
+    
+    for (int line = 0; line < 40; line++)
+        if(strlen((*state).message[line]) != 0)
+            printf("%s\n", (*state).message[line]);
+        
 
     while(true) {
         GRRLIB_PrintfTTF(100, 50, (*state).basicFont, "Error", 32, 0xFFFFFFFF);
 
         for (int line = 0; line < 40; line++)
-        {
             GRRLIB_PrintfTTF(100, 100 + (line * 16), (*state).basicFont, (*state).message[line], 12, 0xFFFFFFFF);
-            printf("%s\n", (*state).message[line]);
-        }
 
         WPAD_ScanPads();
         u32 pressed = WPAD_ButtonsDown(0);
