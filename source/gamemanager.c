af8618914a0e3f8de2da0793951763a22bc591f2
Martin Sundhaug <martinsundhaug@gmail.com>
2025-03-30 18:30:03 +0000

    Force GX draw done on screen change

--- source/gamemanager.c
+++ source/gamemanager.c
@@ -60,6 +60,8 @@ int main(int argc, char **argv)
             currentMode = error_screen(&global_state);
             break;
         }
+        
+        GX_WaitDrawDone();
     }
 
     GRRLIB_Exit();
